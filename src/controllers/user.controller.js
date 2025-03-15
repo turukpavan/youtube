9c81c4e87955e628d28b30071d1dad27118faeae
Pavan Turuk <turukpavan@gmail.com>
2025-03-15 16:10:46 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -196,7 +196,7 @@ const logoutUser = asyncHandler(async(req,res)=>{
 
 const refreshAccessToken = asyncHandler(async ()=>{
   const incomingRefreshToken =  req.cookie.refreshToken || req.body.refreshToken 
-  if(incomingRefreshToken){
+  if(!incomingRefreshToken){
    throw new ApiError(401,"unauthorized request");
   }
   try {
